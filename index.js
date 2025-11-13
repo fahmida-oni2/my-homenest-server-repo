b4354532a44192c8cad9be9fd4ed42105ea9e215
fahmida-oni2 <onifahmida@gmail.com>
2025-11-13 07:22:24 +0600

    added the update method

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require("dotenv").config()
-const app = express()
-const port = 3000
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pwy4qnn.mongodb.net/?appName=Cluster0`;
 
@@ -15,65 +15,80 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
-   const db = client.db('homenest-db')
-   const propertyCollection = db.collection('allproperties')
+    const db = client.db("homenest-db");
+    const propertyCollection = db.collection("allproperties");
 
-    app.get('/allProperties',async(req,res)=>{
-        const result =await propertyCollection.find().toArray()
-        res.send(result)
-    })
+    app.get("/allProperties", async (req, res) => {
+      const result = await propertyCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/properties/:id',async(req,res)=>{
-      const {id} =req.params
-        const result = await propertyCollection.findOne({_id: new ObjectId(id)})
-      
-        res.send(result)
-    })
+    app.get("/allProperties/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await propertyCollection.findOne({
+        _id: new ObjectId(id),
+      });
 
-    app.get('/latest-properties',async(req,res)=>{
-        const cursor = propertyCollection.find().sort({postedDate: -1}).limit(6)
-        const result = await cursor.toArray()
-        res.send(result)
-    })
-     app.get('/my-properties',async(req,res)=>{
-      const email = req.query.email
-        const cursor = propertyCollection.find({postedByEmail: email})
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+      res.send(result);
+    });
 
+    app.put("/allProperties/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const queryId = new ObjectId(id);
+      const filter = { _id: queryId };
+      const update = {
+        $set: data,
+      };
+      const result = await propertyCollection.updateOne(filter, update);
 
-    app.post('/allProperties',async(req,res)=>{
-        const data =req.body
-        const result =await propertyCollection.insertOne(data)
-        res.send({
-      success:true,
-      result
-        } )
-    })
+      res.send(result);
+    });
 
+    app.get("/latest-properties", async (req, res) => {
+      const cursor = propertyCollection
+        .find()
+        .sort({ postedDate: -1 })
+        .limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/my-properties", async (req, res) => {
+      const email = req.query.email;
+      const cursor = propertyCollection.find({ postedByEmail: email });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.post("/allProperties", async (req, res) => {
+      const data = req.body;
+      const result = await propertyCollection.insertOne(data);
+      res.send({
+        success: true,
+        result,
+      });
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
